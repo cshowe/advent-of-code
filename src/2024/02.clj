99886e35de8e376628ec7d23436d30e903b78be3
Caleb Howe <cshowe@>
2024-12-02 19:07:48 -0500

    Tidy functions

--- src/2024/02.clj
+++ src/2024/02.clj
@@ -1,16 +1,16 @@
 (ns aoc.2024.02
   (:require [aoc :as aoc]))
 
-(defn is-safe? [x]
+(defn safe? [x]
   (let [ds (mapv #(apply - %) (partition 2 1 x))
         max-d (reduce max ds)
         min-d (reduce min ds)]
     (or (and (>= min-d 1) (<= max-d 3))
         (and (>= min-d -3) (<= max-d -1)))))
 
-(defn damped-safe? [x]
-  (some is-safe? (for [n (range (count x))] (concat (take n x) (drop (inc n) x)))))
+(defn damped [pred]
+  #(some pred (for [n (range (count %))] (concat (take n %) (drop (inc n) %)))))
 
 (let [data (aoc/read-input 2024 2 :lines-as [[parse-long]])]
-  (println (->> data (filter is-safe?) count)
-           (->> data (filter damped-safe?) count)))
+  (println (->> data (filter safe?) count)
+           (->> data (filter (damped safe?)) count)))
